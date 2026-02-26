40b76f4f3b01f9ebf2349ebb80458ff174c59018
Miles <milesjrose@outlook.com>
2026-02-26 11:55:34 -0800

    refactor: remove unused database initialization code

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
 
-from backend.app.database import Base, engine
-
 app = FastAPI(
     title="Semantic Retrieval Group 18 API",
     description="Semantic Retrieval Group 18 API",
@@ -11,11 +9,6 @@ app = FastAPI(
 # TO DO: Add middle ware
 
 # TO DO: Include routers using app.include_router(router)
-
-
-@app.on_event("startup")
-def on_startup():
-    Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
