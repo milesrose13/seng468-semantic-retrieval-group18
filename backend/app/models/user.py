d9069641b1e5f581a9e2235ae37e9b08930f8771
noahmcconnell2 <noahmcconnell@uvic.ca>
2026-02-26 17:14:51 -0800

    feat: Added model/Users.py as a simple db and barebones /auth/signup endpoint

---
+++ backend/app/models/user.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String
+
+from ..database import Base
+
+#This code is based on https://fastapi.tiangolo.com/tutorial/sql-databases/
+# Syntax understanding from Gemini and https://docs.sqlalchemy.org/en/20/core/type_basics.html
+# For Mapping https://docs.sqlalchemy.org/en/20/orm/mapping_styles.html
+#Remove this after but make sure to make note for Report
+
+
+
+class User(Base):
+    __tablename__ = "users"
+    
+    #The primary key which is the unique id for each user in da DB
+    id = Column(Integer, primary_key = True, index=True)
+    
+    # username
+    username = Column(String, unique=True, index=True, nullable=False)
+    
+    hashed_password = Column(String, nullable=False)
--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,12 +1,59 @@
 from fastapi import Depends, FastAPI, HTTPException, status
 from pwdlib import PasswordHash
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+from ..database import get_db  #import the database.py
+from ..models.user import User
+
+#Notes for documentation, what I'm using to dev:
+#Password hashing and security with FastAPI
+# https://fastapi.tiangolo.com/tutorial/security/oauth2-jwt/#hash-and-verify-the-passwords
+#FastAPI (SQL Relational Databases)
+# https://fastapi.tiangolo.com/tutorial/sql-databases/#create-a-hero
+# More stuff I read for Bigger Apps from FASTAPI
+#https://fastapi.tiangolo.com/tutorial/bigger-applications/
 
 
-class User(BaseModel):
+password_hash = PasswordHash.recommended()
+
+class CreateUser(BaseModel):
     username: str
+    password: str
 
-class UserInDB(User):
-    hashed_password: str
-
+class SignupResponse(BaseModel):
+    message : str #User Created Successfully
+    user_id : int | str # STR for UUID or int
+    
 app = FastAPI()
+
+@app.post("/auth/signup")
+async def signup(user_in: CreateUser, db: Session = Depends(get_db)):
+    #TODO: Write code to check if user is already in database
+    #existing_user = db.query(User).filter(User.username == user_in.username).first()
+    #if exisiting_user:
+    # throw the HTTPException TODO: Look into throwing HTTPExcetpion
+    
+    #After checking if the username isnt a duplicate then I can hash the password they put in
+    hashed_pass = password_hash.hash(user_in.password)
+    
+    #Then can create the new User
+    new_user = User(
+        username = user_in.username,
+        hashed_password = hashed_pass
+    )
+    #Add to the user, no idea if this is correct right now, TODO Look more into
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    
+    # Response (200 OK) From Project Description
+    return {
+        "message" : "User Created Successfully",
+        "user_id" : new_user.id
+    }
+    
+    
+    
+        
+            
